0cb1a06fe762c1dceb69cd3d9f48c8ad980bcc5e
JuanBuendia <juan.buendia@usantoto.edu.co>
2020-06-08 23:47:52 -0500

    Query; citations for current day

--- src/main/java/com/iris/repositories/CitaRepository.java
+++ src/main/java/com/iris/repositories/CitaRepository.java
@@ -2,8 +2,11 @@ package com.iris.repositories;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.ArrayList;
+
 import com.iris.entities.Cita;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
@@ -12,4 +15,7 @@ public interface CitaRepository extends CrudRepository<Cita, Integer>{
 	public Cita findByIdCita(Integer idCita);
 	public Cita findByHora(Time hora);
 	public Cita findByFecha(Date fecha);
+	
+	@Query(value = "SELECT * FROM CITAS WHERE CURRENT_DATE = FECHA", nativeQuery = true)
+	ArrayList<Cita> todayCitations();
 }
